5596465f6cdd4117b14bfa4bf8e43f8b2478269b
Tom Wilczak <tom.wilczak@gmail.com>
2018-08-22 11:16:39 -0700

    Add drone-png-stream

--- flight.js
+++ flight.js
@@ -1,26 +1,7 @@
 const arDrone = require('ar-drone');
 const client = arDrone.createClient();
-const PaVEParser = require('./node_modules/ar-drone/lib/video/PaVEParser');
-const output = require('fs').createWriteStream('./vid.h264');
-
+const dronePngStream = require('ar-drone-png-stream')(client, { port: 8000 });
 client.config('control:altitude_max', 3000);
-
-const video = client.getVideoStream();
-// video.on('data', console.log);
-video.on('error', console.log);
-
-const parser = new PaVEParser();
-
-parser
-  .on('data', function(data) {
-    console.log(data);
-    output.write(data.payload);
-  })
-  .on('end', function() {
-    output.end();
-  });
-
-video.pipe(parser);
 
 client.takeoff();
 
