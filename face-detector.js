016c391d1dce527ba36bd578d44908c0a27a2f83
Tom Wilczak <tom.wilczak@gmail.com>
2018-08-22 14:01:55 -0700

    Serve page from single server

--- face-detector.js
+++ face-detector.js
@@ -1,14 +1,15 @@
-window.onload = () => {
-    var myImage = new Image();
-    myImage.src = 'http://localhost:8000';
-    var faceDiv = document.getElementById('faces');
+const myImage = document.getElementById('img');
+const faceDiv = document.getElementById('faces');
 
-    var faceDetector = new window.FaceDetector();
-    faceDetector.detect(myImage)
-        .then((faces) => {
-            faces.map((face) => {
-                var box = face.boundingBox
-                faceDiv.setAttribute('style',`position: absolute; top: ${box.top}; left: ${box.left};right: ${box.right};bottom: ${box.bottom};border: 1px yellow`)
-            })
-        }).catch(console.log)
-}
+myImage.onload = () => {
+  const faceDetector = new window.FaceDetector();
+  faceDetector.detect(myImage)
+    .then((faces) => {
+      faces.map((face) => {
+        const box = face.boundingBox;
+        faceDiv.setAttribute('style',`position: absolute; top: ${box.top}; left: ${box.left};right: ${box.right};bottom: ${box.bottom};border: 1px yellow`);
+      })
+    }).catch(console.error);
+};
+
+myImage.src = 'http://localhost:8000/image';
--- flight.js
+++ flight.js
@@ -1,20 +1,21 @@
 const arDrone = require('ar-drone');
 const http = require('http');
+const fs = require('fs');
 const client = arDrone.createClient();
 client.config('control:altitude_max', 3000);
 
 function fly(client) {
-  client.takeoff();
-  
-  client
-    .after(5000, function() {
-      this.clockwise(0.5);
-    })
-    .after(1000, function() {
-      this.stop();
-      this.land();
-    });
-  
+  // client.takeoff();
+  //
+  // client
+  //   .after(5000, function() {
+  //     this.clockwise(0.5);
+  //   })
+  //   .after(1000, function() {
+  //     this.stop();
+  //     this.land();
+  //   });
+  //
 }
 
 function server(client, opts, onLoad) {
@@ -32,21 +33,34 @@ function server(client, opts, onLoad) {
       });
     }
 
-    res.writeHead(200, { 'Content-Type': 'multipart/x-mixed-replace; boundary=--daboundary' });
+    const requestUrl = req.url;
 
-    png.on('data', (d) => {
-      if(!loaded){
-        loaded = true;
-        console.log('read to fly');
-        onLoad();
-      }
-      sendPng(d)
-    });
+    if(requestUrl === '/image') {
+      png.on('data', (data) => {
 
-    function sendPng(buffer) {
-      console.log(buffer.length);
-      res.write('--daboundary\nContent-Type: image/png\nContent-length: ' + buffer.length + '\n\n');
-      res.write(buffer);
+        if(!loaded){
+          loaded = true;
+          console.log('read to fly');
+          onLoad();
+        }
+
+        console.log(data.length);
+        res.writeHead(200, {'Content-Type': 'multipart/x-mixed-replace; boundary=--daboundary',});
+        res.write('--daboundary\nContent-Type: image/png\nContent-length: ' + data.length + '\n\n');
+        res.write(data);
+      });
+    } else if(requestUrl === '/') {
+      fs.readFile('./index.html',function (err, data){
+        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+        res.write(data);
+        res.end();
+      });
+    } else {
+      fs.readFile('./face-detector.js',function (err, data){
+        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+        res.write(data);
+        res.end();
+      });
     }
   });
 
