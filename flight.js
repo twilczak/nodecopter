4f26d2d7afb8b47db32457dc27b46245debf6211
Tom Wilczak <tom.wilczak@gmail.com>
2018-08-22 10:53:25 -0700

    Add save video code

--- flight.js
+++ flight.js
@@ -1,15 +1,31 @@
 const arDrone = require('ar-drone');
 const client = arDrone.createClient();
+const PaVEParser = require('./node_modules/ar-drone/lib/video/PaVEParser');
+const output = require('fs').createWriteStream('./vid.h264');
 
 client.config('control:altitude_max', 3000);
+
+const video = client.getVideoStream();
+video.on('data', console.log);
+video.on('error', console.log);
+
+const parser = new PaVEParser();
+
+parser
+  .on('data', function(data) {
+    output.write(data.payload);
+  })
+  .on('end', function() {
+    output.end();
+  });
+
+video.pipe(parser);
+
 client.takeoff();
 
 client
   .after(5000, function() {
     this.clockwise(0.5);
-  })
-  .after(3000, function() {
-    this.animate('flipLeft', 15);
   })
   .after(1000, function() {
     this.stop();
